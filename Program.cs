0ef451e526ec0bb3f5a7fd464c4d0f85281d4691
secretoftheooze <brandonroot@trentu.ca>
2016-02-17 18:46:28 -0500

    Adding the cs files is a great idea

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Scheduler
+{
+    class Program
+    {
+        const int NUM_TESTS = 20;   // The number of tests being done
+        const int NUM_TASKS = 10;   // The number of jobs being generated for processing 
+
+        static void Main(string[] args)
+        {
+
+            // Testing the queue generator, single test
+            Queue<Task> taskQ = generateTasks(NUM_TASKS);
+            Task[] tasks = taskQ.ToArray();
+
+            for (int i = 0; i < taskQ.Count; i++)
+            {
+                Console.WriteLine(String.Format("Task Number: {0}, Run Time: {1}, Arrival Time: {2}", i, tasks[i].RunTime, tasks[i].ArriveTime));
+            }
+
+            Console.ReadLine();
+
+        }
+
+        // Generate Tasks
+        // Generates a queue of tasks of length numTasks, with run times and arrival times randomly generated
+        private static Queue<Task> generateTasks(int numTasks)
+        {
+            Queue<Task> taskQ = new Queue<Task>();
+            Random rando = new Random(); // Initializes random number generator
+            int arriveAcc = 0; // Initialize arrive time accumulator
+
+            // Generate each task and add it to the queue
+            // The first task arrives at 0, and then every task after that arrives 5-8 units after the previous one
+            for(int i = 0; i < numTasks; i++)
+            {
+                taskQ.Enqueue(new Task(rando.Next(2,60), arriveAcc));           // Queue a new task with a random run time of 2-60 units and a arrival time every 5-8 seconds
+                arriveAcc = arriveAcc + rando.Next(5, 8);                       // Tasks arrive every 5-8 units of time after previous task
+            }
+
+            return taskQ;
+
+        }
+    }
+}
---
+++ Task.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+
+namespace Scheduler
+{
+    class Task
+    {
+        // Values
+        int runTime;        // Time it takes to process the task
+        int arriveTime;     // The time the task arrives to be processed
+        int startTime;      // The time that the task starts processing
+        int timeLeft;       // The time left for the task to process, initially set as runTime and ticked down from there
+        int endTime;        // The time that the task finished processing
+
+        // Stride specific values
+        int tickets;        // How many tickets are alloted to the task for Stride scheduling
+        int stride;         // Stride value, determined by stride algorithm
+        int passCount;      
+
+
+        // Constructor
+        // Set initial values for run time and arrival time
+        public Task (int runTime, int arriveTime)
+        {
+            this.runTime = runTime;
+            this.arriveTime = arriveTime;
+
+            timeLeft = runTime; // Sets the initial value for timeLeft
+        }
+
+        // Properties
+        // Read-only
+        public int RunTime { get { return runTime; } }      // Read-only property for the time it takes to process the task
+        public int ArriveTime { get { return arriveTime; } }   // Read-only property for the time the task arrives to be processed
+
+        // Read-Write
+        // To be manipulated by the algorithms
+        public int StartTime
+        {
+            get { return startTime; }
+            set { startTime = value;  }
+        }
+
+        public int TimeLeft
+        {
+            get { return timeLeft; }
+            set { timeLeft = value; }
+        }
+
+        public int EndTime
+        {
+            get { return endTime; }
+            set { endTime = value; }
+        }
+
+        // Stride specific properties
+        public int Tickets
+        {
+            get { return tickets; }
+            set { tickets = value; }
+        }
+
+        public int Stride
+        {
+            get { return stride; }
+            set { stride = value; }
+        }
+
+        public int PassCount
+        {
+            get { return passCount; }
+            set { passCount = value; }
+        }
+
+    }
+}
