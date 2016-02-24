b6cf8246f9e5c39e07181cc6909831d363c40e47
secretoftheooze <brandonroot@trentu.ca>
2016-02-23 22:20:36 -0500

    Here is another terrible attempt at FIFO , but at least the other issues are dealt with

--- FIFO.cs
+++ FIFO.cs
@@ -23,62 +23,42 @@ namespace Scheduler
 
             // Count the number of tasks that need to be completed
             int taskCount = taskQ.Count;
-            int jIndex;      // Keeps track of the index of the current job in the waiting list 
             int currentRT;  // Keeps track of current tasks runtime
-            
-            foreach (int x in taskQ)
+
+
+            while (completedTasks.Count < taskCount)
             {
 
-                // Check if there is a current process
                 if (currentProcess.StartTime == -1 && taskQ.Count > 0)
                 {
-
-                    // Initialize to the first task's run time 
-                    currentRT = taskQ[0].RunTime;
-                    jIndex = 0;
-
-
-                    for (int i = 1; i < taskQ.Count - 1; i++)
-                    {
-
-                        currentRT = taskQ[i].RunTime;
-                        jIndex = i;
+                    //waitingQ.Enqueue(taskQ.Dequeue());
+                    currentProcess = taskQ.Dequeue();
+                currentProcess.StartTime = clock;
+                currentRT = currentProcess.RunTime;
 
 
+                        // Check if task is done and add it to completedTasks list
+                        if (currentProcess.TimeLeft == 0)
+                        {
+                            currentProcess.EndTime = clock;         // Set end time for the job
+                            completedTasks.Add(currentProcess);     // Add it to the completed task list
+                            currentProcess = new Task();            // Resets the current process
+                        }
+                        else
+                        {
+                            // Process the current task
+                            currentProcess.TimeLeft--;
+                        }
 
-                        // Set current process to current job, and initialize the task by setting its start time
-                        currentProcess = taskQ[jIndex];
-                        currentProcess.StartTime = clock;
-
-                        // Remove the current process from the waiting list
-                        waitingList.Remove(waitingList[jIndex]);
-
+                        // Increment clock and reset the loop
+                        clock++;
                     }
                 }
 
-
-                else
-                {
-                    // Process the current task
-                    currentProcess.TimeLeft--;
-                }
-
-
-                // Check if task is done and add it to completedTasks list
-                if (currentProcess.TimeLeft == 0)
-                {
-                    currentProcess.EndTime = clock;         // Set end time for the job
-                    completedTasks.Add(currentProcess);     // Add it to the completed task list
-                    currentProcess = new Task();            // Resets the current process
-                }
-
-                // Increment clock and reset the loop
-                clock++;
+                // After processing, output the results
+                OutputTest(completedTasks);
             }
-
-            // After processing, output the results
-            OutputTest(completedTasks);
-        }
+        
 
         public new void OutputResults()
         {
